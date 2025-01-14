d8c16c59e3b7783d481b3e6ef6b51f0d6d6139e7
Alejandro2745 <sanchezbolanoscristian@gmail.com>
2025-01-14 17:30:05 -0500

    Notacion @Qualifier para manejo de multiples Beans

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,8 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.Product;
 import com.example.demo.service.ProductService;
-import com.example.demo.service.ProductsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +15,9 @@ import java.util.List;
 @RequestMapping("/productos")
 public class ProductController {
 
+    // Inyeccion de dependecia
     @Autowired
+    @Qualifier("jsonResourceService")
     private  ProductService productsService;
 
     @GetMapping
--- src/main/java/com/example/demo/service/ProductsServiceImpl.java
+++ src/main/java/com/example/demo/service/ProductsServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@Service
+@Service("listResourceService")
 public class ProductsServiceImpl implements ProductService{
 
     List<Product> products = new ArrayList<>(Arrays.asList(
